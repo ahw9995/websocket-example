9750c82f507b4b1aec64486355bc8b98a3e45e6e
HyungWookAhn <HyungWookAhn@MacbookAir.local>
2018-06-25 22:47:22 +0900

    initial source code

---
+++ src/main/java/com/example/websocket/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.example.websocket.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
